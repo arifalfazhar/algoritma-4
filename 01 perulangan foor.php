fa6bca3e9564aa1c5d18466ec20d1aa8be9a1a2b
fachri rizki <fachrir467@gmail.com>
2024-09-17 12:17:15 +0700

    comit algoritma 4

---
+++ 01 perulangan foor.php
@@ -0,0 +1,8 @@
+<?php
+
+for($i = 0; $i <= 10; $i++){
+    echo "<h2>Ini perulangan ke-$i</h2>";
+}
+
+echo "<i><p><strong> By : arif alfazhar";
+?>
---
+++ 37 foreach dalam foreach.php
@@ -0,0 +1,20 @@
+<?php
+$no = 1;
+$tabel = '
+<table>
+	<tr>
+		<th>No</th>
+		<th>Bulan</th>
+		<th>Penjualan</th>
+	</tr>';
+foreach ($sales as $bulan => $nilai) {
+	$tabel .= '
+	<tr>
+		<td>' . $no . '</td>
+		<td>' . $bulan . '</td>
+		<td>' . $nilai . '</td>
+	</tr>';
+	$no++;
+}
+$tabel .= '</table>';
+echo $tabel;
